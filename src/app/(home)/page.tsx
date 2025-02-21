5f026adc41084cc73693f0253345a99fc9d88f1c
Martin <karag2006@gmx.net>
2025-02-21 11:42:43 +0100

    Video Categories

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,22 @@
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
+import { HomeView } from "@/modules/home/ui/views/home-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
-import { PageClient } from "./client";
+export const dynamic = "force-dynamic";
 
-export default async function Home() {
-  void trpc.hello.prefetch({ text: "Martin" });
+interface PageProps {
+  searchParams: Promise<{ categoryId?: string }>;
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { categoryId } = await searchParams;
+
+  void trpc.categories.getMany.prefetch();
 
   return (
     <HydrateClient>
-      <Suspense fallback={<div>Loading ....</div>}>
-        <ErrorBoundary fallback={<div>failed to load</div>}>
-          <PageClient />
-        </ErrorBoundary>
-      </Suspense>
+      <HomeView categoryId={categoryId} />
     </HydrateClient>
   );
-}
+};
+
+export default Page;
